47ef725b38685cabb6984aee81f2e9b3bda6b851
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2023-11-22 18:14:22 +0200

    basic class implementation for User and Customer Classes

--- OOP23_Food Ordering System/src/project/classes/user/User.java
+++ OOP23_Food Ordering System/src/project/classes/user/User.java
@@ -1,4 +1,52 @@
 package project.classes.user;
 
-public class User {
+public abstract class User
+{
+    protected String username;
+    protected String password;
+    protected String email;
+
+    public User()
+    {
+        this.username = null;
+        this.password = null;
+        this.email = null;
+    }
+
+    public User(String username, String password, String email)
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
