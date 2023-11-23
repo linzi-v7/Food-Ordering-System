d57f57c826056c108a890f1e19e740fffc32b26c
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2023-11-23 14:01:36 +0200

    basic main function for now

---
+++ OOP23_Food Ordering System/src/project/main/main.java
@@ -0,0 +1,33 @@
+package project.main;
+
+import java.util.Scanner;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("######## Food Ordering System #############\n");
+        System.out.println("\t\t\t Welcome!\n");
+        System.out.println("Do you already have an account? Enter Yes or No");
+
+        String isUserCheck = scanner.nextLine();
+
+        if(      isUserCheck.equalsIgnoreCase("yes")
+                || isUserCheck.equalsIgnoreCase("y"))
+        {
+          //login();
+        }
+        else if (isUserCheck.equalsIgnoreCase("no")
+                || isUserCheck.equalsIgnoreCase("n"))
+        {
+            //register();
+        }
+        else
+        {
+            System.out.println("Invalid Output");
+        }
+
+    }
+}
