4d282d99abf00d30028cb7b98d5e2fc6b0317930
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2023-11-26 14:58:26 +0200

    fixed check duplicate email function

--- OOP23_Food Ordering System/src/project/user/RegistrationSystem.java
+++ OOP23_Food Ordering System/src/project/user/RegistrationSystem.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 public class RegistrationSystem
 {
     //file for storing users data
+    //file must not end with \n on a seperate line to avoid exceptions
+    //if file ended with \n on a seperate line, the userdata array thinks its a row that contains user
+    //data which throws an arrayoutofbounds exception.
     private static final String USER_FILE = "users.txt";
     public void registerUser() {
         //get input
@@ -16,7 +19,6 @@ public class RegistrationSystem
 
         String email;
         int retryCount = 0;
-        //THIS FUNCTION IS NOT WORKING YET.
         do {
             if(retryCount >=  1)
             {
@@ -61,22 +63,31 @@ public class RegistrationSystem
 
        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(USER_FILE)))
        {
-           String [][] usersArray = new String[10][3];
+           String [][] usersArray = new String[10][4]; //array to load user data from file to array
            String line;
            int row = 0;
-           while((line = bufferedReader.readLine()) != null)
+           while((line = bufferedReader.readLine()) != null) //reads each line until there is none
            {
-               String[] values = line.split(";");
+               String[] values = line.split(";"); //needs to split them to remove the delimiter
                usersArray[row] = values;
                row++;
            }
 
            for (int i =0; i<10 ;i++)
            {
-              if(usersArray[i][2].equals(email))
-              {
-                  return true; // found duplicate email;
-              }
+               try {
+                   if (usersArray[i][2].equals(email))
+                   {
+                       return true; // found duplicate email
+                   }
+               }catch(ArrayIndexOutOfBoundsException exp) //if file ended with \n on a seperate line
+               {
+                   return false;
+               }
+               catch(NullPointerException exp) //if reached the end of the array
+               {
+                   return false;
+               }
 
            }
 
