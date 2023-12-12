d3e6a70de5dcdb593b8bb6c461951b07f77b48a7
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2023-12-12 10:04:56 +0200

    dish (kareem)

--- OOP23_Food Ordering System/src/project/menu/Dish.java
+++ OOP23_Food Ordering System/src/project/menu/Dish.java
@@ -1,32 +1,54 @@
 package project.menu;
 
-public class Dish {
+import java.util.ArrayList;
+class Dishes {
     private String name;
     private String description;
     private double price;
     private String additionalOption;
 
-    public Dish(String name, String description, double price, String additionalOption)
+    public Dishes(String name, String description, double price, String additionalOption)
     {
         this.name = name;
         this.description = description;
         this.price = price;
-        this.additionalOption=additionalOption;
+        this.additionalOption= additionalOption;
     }
     public String getName() {
+
         return name;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public String getAdditionalOption() {
+
         return additionalOption;
     }
-
+    public String toString() {
+        return "name:'" + name + "description:" + description + "price$=" + price + "additionalOption:" + additionalOption;
+    }
+}
+class Menu {
+    private ArrayList<Dishes> dish;
+    public Menu() {
+        this.dish = new ArrayList<>();
+    }
+    public void addDish(Dishes Dish) {
+        dish.add(Dish);
+    }
+    public void displayMenu() {
+        System.out.println("Menu:");
+        for (Dishes Dish:dish ) {
+            System.out.println(Dish);
+        }
+    }
 }
