dea9e364df69b1238dc69bb4f15b2b774dc953db
kareemrizk200 <112204322+kareemrizk200@users.noreply.github.com>
2023-12-17 20:46:18 +0200

    Update Dish.java

--- OOP23_Food Ordering System/src/project/menu/Dish.java
+++ OOP23_Food Ordering System/src/project/menu/Dish.java
@@ -1,18 +1,19 @@
 package project.menu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 class Dishes {
     private String name;
     private String description;
     private double price;
-    private String additionalOption;
 
-    public Dishes(String name, String description, double price, String additionalOption)
-    {
+    public Dishes(String name, String description, double price)
+        {
         this.name = name;
         this.description = description;
         this.price = price;
-        this.additionalOption= additionalOption;
     }
     public String getName() {
 
@@ -28,28 +29,40 @@ class Dishes {
 
         return price;
     }
-
-    public String getAdditionalOption() {
-
-        return additionalOption;
-    }
-    //@Override
     public String toString() {
-        return "name:'" + name + "description:" + description + "price$=" + price + "additionalOption:" + additionalOption;
+        return "name:'" + name + "description:" + description + "price$=" + price ;
+                }
+    public static Dishes fromString(String line) {
+        String[] parts = line.split(",");
+        String name = parts[0].trim();
+        String description = parts[1].trim();
+        double price = Double.parseDouble(parts[2].trim());
+        return new Dishes(name, description, price);
+      }
     }
-}
 class Menu {
     private ArrayList<Dishes> dish;
     public Menu() {
         this.dish = new ArrayList<>();
     }
-    public void addDish(Dishes Dish) {
-        dish.add(Dish);
-    }
-    public void displayMenu() {
-        System.out.println("Menu:");
-        for (Dishes Dish:dish ) {
-            System.out.println(Dish);
+    public void loadMenuFromFile(String filePath) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dish.add(Dishes.fromString(line));
+            }
         }
     }
-}
+
+    public void addDish(Dishes Dish) {
+
+
+            dish.add(Dish);
+        }
+        public void displayMenu() {
+            System.out.println("Menu:");
+            for (Dishes Dish : dish) {
+                System.out.println(Dish);
+            }
+        }
+    }
