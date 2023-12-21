fe9727203db9d34d79ade99c452b9687b4aa396a
pierre atef <129335262+pierre10105@users.noreply.github.com>
2023-12-21 17:40:11 +0200

    Update Restaurant.java

--- OOP23_Food Ordering System/src/project/restaurant/Restaurant.java
+++ OOP23_Food Ordering System/src/project/restaurant/Restaurant.java
@@ -1,137 +1,236 @@
-package project.restaurant;
+package project.main;
+import project.cart.cart;
+import project.restaurant.Restaurant;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import project.restaurant.restaurantRegistration;
+import project.admin.Admin;
+import project.review.review;
+import project.user.User;
+import project.user.UserManagementSystem;
+import project.menu.*;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Restaurant {
-    public String restaurantName;
-    public String restaurantAddress;
-    public String restaurantContact;
-    public String restaurantEmail;
-    public String restaurantPassword;
 
-    ArrayList<ArrayList<String>> restaurants;
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        UserManagementSystem.readUserDataFile();
+        Scanner scanner = new Scanner(System.in);
+        String userEmail = null;
+        Restaurant restaurant = new Restaurant();
+        restaurant.loadRestaurantsFromFile("\"C:\\Users\\Omar Fakharany\\IdeaProjects\\restaurant\\restaurants.txt\"");
+        do
+        {
+            System.out.println("\t####### Food Ordering System #######\n");
+            System.out.println("\t\t\t\t Welcome!\n");
+            System.out.println("Do you already have an account? Enter Yes or No\n" +
+                    "Type exit to close program.");
 
-    public Restaurant() {
-        this.restaurants = new ArrayList<>();
+            String isUserCheck = scanner.nextLine();
 
-    }
+            if (isUserCheck.equalsIgnoreCase("yes")
+                    || isUserCheck.equalsIgnoreCase("y"))
+            {
+                String retry;
+                do
+                {
+                    retry = "n";
+                    if ( (userEmail=UserManagementSystem.loginUser(restaurant)).equals("null"))
+                    {
+                        System.out.println("Would you like to retry?(Y/N)");
+                        retry = scanner.nextLine();
+                    }
+                    //prompts user to retry if they entered wrong credentials
+                } while (retry.equalsIgnoreCase("y")
+                        || retry.equalsIgnoreCase("yes"));
 
-    public ArrayList<ArrayList<String>> getRestaurants() {
-        return restaurants;
-    }
+            } else if (isUserCheck.equalsIgnoreCase("no")
+                    || isUserCheck.equalsIgnoreCase("n")) {
+                int decision ;
 
-    public String getRestaurantName() {
-        return restaurantName;
-    }
+                System.out.println("to register as  new user enter: 1\n" +
+                        "to register as a new restaurant enter 2 ");
+                decision= scanner.nextInt();
+                switch (decision)
+                {
+                    case 2:
 
-    public void loadRestaurantsFromFile(String filepath) {
-        try {
-            Scanner fileScanner = new Scanner(new File(filepath));
+                        restaurantRegistration.registerRestaurant(restaurant);
+                        break;
+                    case 1:
+                        UserManagementSystem.registerUser(1);
 
-            while (fileScanner.hasNextLine()) {
-                String line = fileScanner.nextLine();
-                String[] parts = line.split(",");
-                if (parts.length == 5) {
-                    String restaurantName = parts[0].trim();
-                    String restaurantAddress = parts[1].trim();
-                    String restaurantContact = parts[2].trim();
-                    String restaurantEmail = parts[3].trim();
-                    String restaurantPassword = parts[4].trim();
-                    addRestaurantDetails(restaurantName, restaurantContact, restaurantAddress, restaurantEmail, restaurantPassword);
+                        break;
+
+                    default:
+                        System.out.println("please enter a correct number");
                 }
             }
-            fileScanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Error parsing price: " + e.getMessage());
-        }
-
-    }
-
-    public void addRestaurantDetails(String restaurantName, String restaurantContact, String restaurantAddress, String restaurantEmail, String restaurantPassword) {
-        ArrayList<String> restaurantDetails = new ArrayList<>();
-        restaurantDetails.add(restaurantName);
-        restaurantDetails.add(restaurantContact);
-        restaurantDetails.add(restaurantAddress);
-        restaurantDetails.add(restaurantEmail);
-        restaurantDetails.add(restaurantPassword);
-        restaurants.add(restaurantDetails);
-
-    }
-
-
-    public  String login(Restaurant restaurant,String mail,String password) {
-
-        boolean mailValidation, passwordValidation;
-        mailValidation=restaurant.compareEmail(mail);
-        passwordValidation=restaurant.comparePassword(password);
-        boolean sameAccount = verifyAccount(mail,password);
-        if ((mailValidation) && (passwordValidation)&&(sameAccount)) {
-            return mail;
-
-        }
-
-        return "null";
-    }
-
-    boolean compareEmail(String email) {
-        boolean uniqueEmail = false;
-        for (ArrayList<String> row : restaurants) {
-
-            if (row.size() >= 3 && row.get(3).equals(email)) {
-
-                return true; // Found a match
-
+            else if(isUserCheck.equalsIgnoreCase("exit"))
+            {
+                System.exit(0);
             }
-
-        }
-        return uniqueEmail;
-    }
-    boolean comparePassword (String password) {
-        boolean uniquePassword = false;
-        for (ArrayList<String> row : restaurants) {
-
-            if (row.size() >= 4 && row.get(4).equals(password)) {
-                return true; // Found a match
-
+            else
+            {
+                System.out.println("Invalid Input");
             }
+        }while(Role.getRoleIdentifier() == 0);
 
-        }
 
-        return uniquePassword;
-    }
-    public void restaurantSearching(Restaurant restaurant)
-    {
-
-        int counter=0;
-        System.out.println("enter the name of the restaurant you're searching for:");
-        Scanner scanner = new Scanner(System.in);
-        String search = scanner.nextLine();
-        for(ArrayList<String> restaurant1: restaurant.restaurants){
-            if(restaurant1.get(0).startsWith(search)) {
-                counter++;
-                System.out.println(counter + "." + restaurant1.get(0));
-            }
-        }
-
-    }
-    boolean verifyAccount(String mail,String password)
-    {
-        int index = 0;
-        for (ArrayList<String> restaurants2 :restaurants)
+        switch (Role.getRoleIdentifier())
         {
-            if (restaurants2.size()>=3 && restaurants2.get(3).equals(mail))
-                index = restaurants2.indexOf(4);
+            case Role.ADMIN_IDENTIFIER:
+                adminLaunchProgram();
+                break;
+            case Role.RESTAURANT_IDENTIFIER:
+                //restaurantLaunchProgram(restaurant);
+                break;
+            case Role.USER_IDENTIFIER:
+                userLaunchProgram(userEmail,restaurant);
+
         }
-        for (ArrayList<String>restaurant3:restaurants) {
-            if (restaurant3.size() >= 4 && restaurant3.get(4).equals(password))
-                if (index == restaurant3.indexOf(4))
-                    return true;
-        }
-        return false;
+
+
     }
+
+    //function to handle program flow after user logs in, should display restaurants, menus,etc..
+    public static void userLaunchProgram(String userEmail,Restaurant restaurant)
+    {
+        User loggedInUser = UserManagementSystem.getUserByEmail(userEmail);
+
+        //if somehow the user doesn't exist (after multiple checks), we should terminate the program
+        if (loggedInUser != null)
+        {
+            Scanner scanner = new Scanner(System.in);
+            int ChosenRestaurant;
+            System.out.println("\n\t\tWelcome " + loggedInUser.getName() + "!");
+
+            //Display and search restaurant
+
+            System.out.println("select what you want to do and enter the number of the operation that you want.");
+            System.out.println("to display all restaurants enter: 1");
+            System.out.println("to search for a specific restaurant enter: 2");
+//while(true) {
+            int decision = scanner.nextInt();
+            switch (decision) {
+                case 1:
+                    int counter = 0;
+                    for (ArrayList<String> restaurants : restaurant.getRestaurants()) {
+                        counter++;
+                        System.out.println(counter + "." + restaurants.get(0));
+
+                    }
+
+
+                    break;
+
+                case 2:
+                    restaurant.restaurantSearching(restaurant);
+                    break;
+
+                default:
+                    System.out.println("choose a value that exists within the choices");
+
+            }
+
+            System.out.println("enter the number of the restaurant that you want");
+            ChosenRestaurant = scanner.nextInt();
+
+
+             String RestaurantName = restaurant.getRestaurants().get(ChosenRestaurant-1).get(0);
+
+          //  String RestaurantName = "mac";
+
+            //display the menu of selected restaurant
+            Menu menu = new Menu();
+            menu.readMenuDataFile(RestaurantName);
+
+            cart orderCart = new cart();
+            System.out.println("#### " + RestaurantName +"'s Menu ###");
+            menu.displayMenu(RestaurantName);
+
+            // User interaction
+            System.out.print("Enter the item number to add to the cart (0 to finish): ");
+            int choice;
+            while ((choice = scanner.nextInt()) != 0) {
+                if (choice > 0 && choice <= menu.getMenuSize()) {
+                    orderCart.addItem(menu.getMenuItem(choice - 1));
+                } else {
+                    System.out.println("Invalid choice. Try again.");
+                }
+                System.out.print("Enter the item number to add to the cart (0 to finish): ");
+            }
+
+            // Display the order
+            orderCart.displayCart();
+            System.out.println("Total: $" + orderCart.calculateTotal());
+
+
+            //orderProcessing();
+            //paymentProcessing();
+            //orderTracking();
+            //reviewProcessing();
+
+//}
+        }
+        else
+        {
+            System.out.println("USER DOESN'T EXIST!");
+            System.exit(-1);
+        }
+    }
+
+//    public static void PaymentTest(String[] args) {
+//        // Creating a Payment object with initial details
+//        Payment payment = new Payment(123456, "Pending", "");
+//
+//        // Testing payment processing
+//        payment.paymentProcess();
+//
+//        // Displaying the updated payment details
+//        System.out.println("Transaction ID: " + payment.getTransactionId());
+//        System.out.println("Payment Status: " + payment.getPaymentStatus());
+//        System.out.println("Payment Method: " + payment.getPaymentMethod());
+//    }
+    public static void mainreview(String[] args)
+    {
+
+
+        System.out.println("Welcome to the Review System!");
+
+        // Collect user information
+        review userReview = new review();
+        userReview.setName();
+        userReview.setNum_phone();
+        userReview.setRev_rest();
+        userReview.setRev_staf();
+        userReview.setRev_clean();
+        userReview.setRev_food();
+        userReview.setRev_design();
+        userReview.setComments();
+
+        // Specify the file path where you want to save the data
+        String filePath = "D:\\MY_Projects\\Food-Ordering-System\\reviews.txt";
+
+        // Save the data to the file
+        userReview.saveToFile(filePath);
+
+        System.out.println("Review saved successfully. Thank you for your feedback!");
+
+    }
+
+
+    //function that handles program flow after a system administrator logs in
+    public static void adminLaunchProgram()
+    {
+        Admin sysAdmin = new Admin();
+
+        sysAdmin.runDashboard();
+    }
+
+
+
 }
