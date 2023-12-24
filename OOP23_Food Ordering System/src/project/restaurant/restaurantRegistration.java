5c9f27d2d792ad20be5aeae90febfec2dd1c9069
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2023-12-24 11:25:08 +0200

    restaurant

--- OOP23_Food Ordering System/src/project/restaurant/restaurantRegistration.java
+++ OOP23_Food Ordering System/src/project/restaurant/restaurantRegistration.java
@@ -24,56 +24,56 @@ public class restaurantRegistration {
         }
 //
 ////enters the name of the restaurant and checks if he wants to enter the name again
-        //while (true) {
+        while (true) {
 
-//
-//            boolean repeat = true;
-//            while (repeat) {
-//
-//                System.out.println("enter phone number:");
-//
-//                while (!scanner.hasNextInt()) {
-//                    scanner.next();
-//                    // Clear the buffer
-//
-//
-//                    System.out.println("Error: Please enter a valid phone number.");
-//
-//                }
-//                //checks that the phone number is nothing else but numbers
-//
-//                restaurant.restaurantContact = scanner.next();
-//
-//
-//                int compareNumber = restaurant.restaurantContact.length();
-//                if ((compareNumber == 11)||(compareNumber==5)) {
-//                    repeat = false;
-//                } else
-//
-//                    System.out.println("please enter the full number");
-//
-//
-//            }
-//            //checks length of the number
-//            System.out.print("do you want to change the phone number? if not enter any key");
-//            scanner.next();
-//            String decision = scanner.nextLine().toLowerCase();
-//
-//            if (!decision.equals("yes")) break;
-//        }
-//
-////enters the number of the restaurant and checks if he wants to enter it again
-////         checks the length of the phone number
-////            checks if input is a number
-//
-//        while (true) {
-//
-//            System.out.println("Enter restaurant address: ");
-//            restaurant.restaurantAddress = scanner.nextLine();
-//            System.out.print("do you want to change the restaurant address? if not enter any key");
-//            String decision = scanner.nextLine().toLowerCase();
-//            if (!decision.equals("yes")) break;
-//        }
+
+            boolean repeat = true;
+            while (repeat) {
+
+                System.out.println("enter phone number:");
+
+                while (!scanner.hasNextInt()) {
+                    scanner.next();
+                    // Clear the buffer
+
+
+                    System.out.println("Error: Please enter a valid phone number.");
+
+                }
+                //checks that the phone number is nothing else but numbers
+
+                restaurant.restaurantContact = scanner.next();
+
+
+                int compareNumber = restaurant.restaurantContact.length();
+                if ((compareNumber == 11)||(compareNumber==5)) {
+                    repeat = false;
+                } else
+
+                    System.out.println("please enter the full number");
+
+
+            }
+            //checks length of the number
+            System.out.print("do you want to change the phone number? if not enter any key");
+            scanner.next();
+            String decision = scanner.nextLine().toLowerCase();
+
+            if (!decision.equals("yes")) break;
+        }
+
+//enters the number of the restaurant and checks if he wants to enter it again
+//         checks the length of the phone number
+//            checks if input is a number
+
+        while (true) {
+
+            System.out.println("Enter restaurant address: ");
+            restaurant.restaurantAddress = scanner.nextLine();
+            System.out.print("do you want to change the restaurant address? if not enter any key");
+            String decision = scanner.nextLine().toLowerCase();
+            if (!decision.equals("yes")) break;
+        }
 
 //enters the address of the restaurant and checks if he wants to enter it again
         boolean validEmail;
